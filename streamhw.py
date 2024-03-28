a7cb51967ac87678dee276f9db6d1a00d34d6475
PolinaBoeva <72100623+PolinaBoeva@users.noreply.github.com>
2024-03-28 22:09:32 +0300

    streamhw.py

--- streamhw.py
+++ streamhw.py
@@ -13,9 +13,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from streamlit_jupyter import StreamlitPatcher, tqdm
 
-StreamlitPatcher().jupyter()  # register streamlit with jupyter-compatible wrappers
 
 
 # In[3]:
